28fc48f8829a60a2dd26d2954b4edb663688b3b8
Steven Fields <stevenjfields97@gmail.com>
2023-02-20 16:56:26 -0600

    Create milvus schema on startup if it doesn't exist

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 import uvicorn
+from pymilvus import utility
+
 from app.api import app
+from app.constants import COLLECTION_NAME
+from app.milvus_shema import establish_connection, create_collection
 
 if __name__ == '__main__':
+    # Create Milvus Schema if it doesn't already exist
+    establish_connection()
+    if not utility.has_collection(COLLECTION_NAME):
+        create_collection()
+
     uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level='info', reload=True)
