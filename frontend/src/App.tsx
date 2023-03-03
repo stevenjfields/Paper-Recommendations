a4d65c3ddbfbfe71de018b47d731dd17da72f8f3
Steven Fields <stevenjfields97@gmail.com>
2023-03-03 15:28:50 -0600

    Update frontend to work with typescript, pretending I know what I'm doing with frontend stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
 function App() {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <App/>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
