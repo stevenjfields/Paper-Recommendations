3ade58a0efd6756d04ac9975dcc6fea84eccad90
Steven Fields <stevenjfields97@gmail.com>
2023-03-07 16:54:39 -0600

    Revert "Update frontend to work with typescript, pretending I know what I'm doing with frontend stuff"
    This reverts commit a4d65c3ddbfbfe71de018b47d731dd17da72f8f3.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <App/>
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
