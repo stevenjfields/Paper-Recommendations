d525709df5e85573bbf3e94af037f24ced8c6795
Steven Fields <stevenjfields97@gmail.com>
2023-04-04 15:29:46 -0500

    typo

--- backend/frontend/resources/javascript/globals.js
+++ backend/frontend/resources/javascript/globals.js
@@ -8,7 +8,7 @@ let search_term = '';
 let selected_paper_id = '';
 let papers;
 
-// Open Alex has a rate limit of 10 per second, setting this to 2 shows now errors on my machine,
+// Open Alex has a rate limit of 10 per second, setting this to 2 shows no errors on my machine,
 // but at 3 I start getting limited.
 const CONCURRENCY_LIMIT = 2;
 const DEBUG = true;
